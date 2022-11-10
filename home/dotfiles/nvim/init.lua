271b385670e3259fb552ee19fb1c242899375474
3rd <3rd@users.noreply.github.com>
2022-11-10 19:34:38 +0200

    profiling config & loading improvements

--- home/dotfiles/nvim/init.lua
+++ home/dotfiles/nvim/init.lua
@@ -1,4 +1,19 @@
+local has_impatient, impatient = pcall(require, "impatient")
 local lib = require("lib")
+
+local profile = {
+  lua = false,
+  impatient = false,
+  packer = false,
+}
+
+if profile.lua then
+  require("profiler").configuration({ fW = 60, fnW = 20, lW = 20 })
+  require("profiler").start()
+end
+if profile.impatient then
+  if has_impatient then impatient.enable_profile() end
+end
 
 require("modules/options").setup()
 require("modules/mappings").setup()
@@ -6,7 +21,13 @@ require("modules/mappings").setup()
 lib.packer.init()
 lib.module.load_modules()
 for _, plugin in ipairs({ "syslang" }) do
-  local plugin_path = string.format("%s/%s", string.format("%s/plugins", vim.fn.stdpath("config")), plugin)
+  local plugin_path =
+    string.format("%s/%s", string.format("%s/plugins", vim.fn.stdpath("config")), plugin)
   lib.packer.register_plugin({ plugin_path })
 end
-lib.packer.load()
+lib.packer.load(profile.packer)
+
+if profile.lua then
+  require("profiler").stop()
+  require("profiler").report("profiler.log")
+end
