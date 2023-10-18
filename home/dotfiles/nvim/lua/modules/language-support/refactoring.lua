61e9e99a98f445cfe3510fdfedf71b5d9ed93f3e
3rd <3rd@users.noreply.github.com>
2023-10-19 00:31:22 +0300

    nvim: setup refactoring

--- home/dotfiles/nvim/lua/modules/language-support/refactoring.lua
+++ home/dotfiles/nvim/lua/modules/language-support/refactoring.lua
@@ -4,8 +4,46 @@ local setup = function()
   vim.keymap.set(
     { "n", "v" },
     "<leader>ar",
-    ":lua require('refactoring').select_refactor()<CR>",
+    ":lua require('refactoring').select_refactor()<cr>",
     { noremap = true, silent = true, expr = false, desc = "Refactor" }
+  )
+
+  vim.keymap.set(
+    { "n", "v" },
+    "<leader>ef",
+    ":Refactor extract ",
+    { noremap = true, silent = true, expr = false, desc = "Extract function" }
+  )
+  vim.keymap.set(
+    { "n", "v" },
+    "<leader>ev",
+    ":Refactor extract_var ",
+    { noremap = true, silent = true, expr = false, desc = "Extract variable" }
+  )
+  vim.keymap.set(
+    { "n", "v" },
+    "<leader>if",
+    ":Refactor inline_func",
+    { noremap = true, silent = true, expr = false, desc = "Inline function" }
+  )
+  vim.keymap.set(
+    { "n", "v" },
+    "<leader>iv",
+    ":Refactor inline_var",
+    { noremap = true, silent = true, expr = false, desc = "Inline function" }
+  )
+
+  vim.keymap.set(
+    { "n", "v" },
+    "<leader>p",
+    ":lua require('refactoring').debug.print_var()<cr>",
+    { noremap = true, silent = true, expr = false, desc = "Print var" }
+  )
+  vim.keymap.set(
+    { "n", "v" },
+    "<leader>P",
+    ":lua require('refactoring').debug.cleanup({})<cr>",
+    { noremap = true, silent = true, expr = false, desc = "Clear var prints" }
   )
 end
 
@@ -14,6 +52,7 @@ return lib.module.create({
   plugins = {
     {
       "ThePrimeagen/refactoring.nvim",
+      event = "VeryLazy",
       dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
