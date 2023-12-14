fab4e9175a5713cdf092099218f59260d195a069
3rd <3rd@users.noreply.github.com>
2023-12-15 00:03:55 +0200

    nvim: ts filter go to definition same-line duplicates + ts errors

--- home/dotfiles/nvim/lua/modules/language-support/languages/typescript.lua
+++ home/dotfiles/nvim/lua/modules/language-support/languages/typescript.lua
@@ -82,20 +82,33 @@ return lib.module.create({
             client.server_capabilities.documentRangeFormattingProvider = false
             client.handlers["textDocument/definition"] = function(err, result, ...)
               local patched_result = {}
+              local target_path_line_map = {}
+
+              -- filter line duplicates and external modules if there are internal definitions
               if (vim.tbl_islist(result) or type(result) == "table") and #result > 1 then
                 local internal_entries = {}
                 local external_entries = {}
                 for _, v in ipairs(result) do
-                  if vim.fn.stridx(v.targetUri, "node_modules") == -1 then
-                    table.insert(internal_entries, v)
-                  else
-                    table.insert(external_entries, v)
+                  local target_path = v.targetUri
+                  local target_line = v.targetRange.start.line
+
+                  if not target_path_line_map[target_path] then target_path_line_map[target_path] = {} end
+                  local mapped_target_lines = target_path_line_map[target_path]
+
+                  if not mapped_target_lines[target_line] then
+                    mapped_target_lines[target_line] = true
+                    if vim.fn.stridx(target_path, "node_modules") == -1 then
+                      table.insert(internal_entries, v)
+                    else
+                      table.insert(external_entries, v)
+                    end
                   end
                 end
                 patched_result = vim.tbl_isempty(internal_entries) and external_entries or internal_entries
               else
                 patched_result = result
               end
+
               vim.lsp.handlers["textDocument/definition"](err, patched_result, ...)
             end
 
@@ -123,6 +136,17 @@ return lib.module.create({
       opts = {
         highlight = "Type",
         multi_line = true,
+      },
+    },
+    {
+      "OlegGulevskyy/better-ts-errors.nvim",
+      dependencies = { "MunifTanjim/nui.nvim" },
+      ft = filetypes,
+      config = {
+        keymaps = {
+          toggle = "<leader>dd", -- default '<leader>dd'
+          go_to_definition = "<leader>dx", --default '<leader>dx'
+        },
       },
     },
   },
