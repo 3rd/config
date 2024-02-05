48c9abd0c52e817da9d9062dd1ef97700ef8be3b
3rd <3rd@users.noreply.github.com>
2024-02-05 03:29:39 +0200

    nvim: mvp for displaying nested total worked time per folded ts node

--- home/dotfiles/nvim/lua/modules/workflow/folds.lua
+++ home/dotfiles/nvim/lua/modules/workflow/folds.lua
@@ -3,7 +3,7 @@ local get_upper_fold_level = function()
   local foldlevel = -1
   local ok = pcall(vim.cmd, "keepjumps normal! [z")
   if ok then foldlevel = vim.fn.foldlevel(vim.fn.line(".")) end
-  vim.fn.winrestview(winview)
+  if winview then vim.fn.winrestview(winview) end
   return foldlevel
 end
 
@@ -19,8 +19,8 @@ end
 local setup_ufo = function()
   local config = {
     -- folded_info_prefix = " ",
-    -- folded_info_prefix = "... ",
-    folded_info_prefix = "…  ",
+    folded_info_prefix = "... ",
+    -- folded_info_prefix = "…  ",
     show_folded_line_count = false,
     chars = {
       outline = "◉",
@@ -76,11 +76,97 @@ local setup_ufo = function()
     return chunks
   end
 
+  local get_range_meta = function(start_line, end_line)
+    local parser = vim.treesitter.get_parser(0)
+    local tree = parser:parse()[1]
+    local root = tree:root()
+
+    local result = {
+      default_tasks = 0,
+      active_tasks = 0,
+      done_tasks = 0,
+      total_time = 0,
+    }
+
+    local filter_in_range = function(nodes)
+      local filtered = {}
+      for _, node in ipairs(nodes) do
+        local start_row, _, end_row, _ = node:range()
+        if start_row >= start_line and end_row <= end_line then table.insert(filtered, node) end
+      end
+      return filtered
+    end
+
+    result.default_tasks = #filter_in_range(lib.ts.find_children(root, "task_default", true))
+    result.active_tasks = #filter_in_range(lib.ts.find_children(root, "task_active", true))
+    result.done_tasks = #filter_in_range(lib.ts.find_children(root, "task_done", true))
+
+    local total_time = 0
+    local sessions = filter_in_range(lib.ts.find_children(root, "task_session", true))
+    for _, session in ipairs(sessions) do
+      local datetimerange = lib.ts.find_child(session, "datetimerange", true)
+      if datetimerange then
+        local datetime_nodes = lib.ts.find_children(datetimerange, "datetime", true)
+
+        local start_str = ""
+        local end_str = ""
+
+        -- case 1: (datetimerange (datetime date time) time)
+        -- 2023.12.15 16:48-16:50
+        if #datetime_nodes == 1 then
+          local start_date_node = lib.ts.find_child(datetime_nodes[1], "date", true)
+          local start_time_node = lib.ts.find_child(datetime_nodes[1], "time", true)
+          local end_time_node = lib.ts.find_child(datetimerange, "time", false)
+
+          if start_date_node and start_time_node and end_time_node then
+            local start_date_str = vim.treesitter.get_node_text(start_date_node, 0)
+            local start_time_str = vim.treesitter.get_node_text(start_time_node, 0)
+            local end_time_str = vim.treesitter.get_node_text(end_time_node, 0)
+
+            start_str = start_date_str .. " " .. start_time_str
+            end_str = start_date_str .. " " .. end_time_str
+          end
+        end
+
+        if start_str ~= "" and end_str ~= "" then
+          local start_time = vim.fn.strptime("%Y.%m.%d %H:%M", start_str)
+          local end_time = vim.fn.strptime("%Y.%m.%d %H:%M", end_str)
+          local duration = end_time - start_time
+          if duration == 0 then duration = 30 * 60 end
+          total_time = total_time + duration
+        end
+
+        -- -- TODO: case 2: (datetimerange (datetime date time) (datetime date time))
+        -- -- 2023.12.15 16:48 - 2023.12.15 16:50
+        -- if #datetime_nodes == 2 then
+        --   local start_date_node = lib.ts.find_child(datetime_nodes[1], "date", true)
+        --   local start_time_node = lib.ts.find_child(datetime_nodes[1], "time", true)
+        --   local end_date_node = lib.ts.find_child(datetime_nodes[2], "date", true)
+        --   local end_time_node = lib.ts.find_child(datetime_nodes[2], "time", true)
+        --
+        --   local start_date_str = start_date_node and start_date_node:sexpr()
+        --   local start_time_str = start_time_node and start_time_node:sexpr()
+        --   local end_date_str = end_date_node and end_date_node:sexpr()
+        --   local end_time_str = end_time_node and end_time_node:sexpr()
+        --
+        --   if start_date_str and start_time_str and end_date_str and end_time_str then
+        --     local start_time = vim.fn.strptime(start_date_str .. " " .. start_time_str, "%Y.%m.%d %H:%M")
+        --     local end_time = vim.fn.strptime(end_date_str .. " " .. end_time_str, "%Y.%m.%d %H:%M")
+        --     total_time = total_time + (end_time - start_time)
+        --   end
+        -- end
+      end
+    end
+    result.total_time = total_time
+
+    return result
+  end
+
   ---@diagnostic disable-next-line: unused-local
   local virtual_text_handler = function(originalVirtualTextChunks, start_line, end_line, width, truncate)
     if lib.buffer.current.get_filetype() == "syslang" then
       local virtualTextChunks = slang_conceal(originalVirtualTextChunks)
-      local folded_lines = vim.api.nvim_buf_get_lines(0, start_line, end_line, true)
+      -- local folded_lines = vim.api.nvim_buf_get_lines(0, start_line, end_line, true)
       local folded_lines_count = end_line - start_line
 
       local max_length = 80
@@ -90,22 +176,10 @@ local setup_ufo = function()
       end
 
       -- slang tasks
-      local normal_task_count = 0
-      local active_task_count = 0
-      local completed_task_count = 0
-      for _, current_line in ipairs(folded_lines) do
-        local text = string.trim(current_line)
-        if string.starts_with(text, "[ ]") then
-          normal_task_count = normal_task_count + 1
-        elseif string.starts_with(text, "[x]") then
-          completed_task_count = completed_task_count + 1
-        elseif string.starts_with(text, "[-]") then
-          active_task_count = active_task_count + 1
-        end
-      end
-      local tasks_todo_count = normal_task_count + active_task_count
-      local tasks_done_count = completed_task_count
-      local tasks_total_count = normal_task_count + active_task_count + completed_task_count
+      local meta = get_range_meta(start_line, end_line)
+      local tasks_todo_count = meta.default_tasks + meta.active_tasks
+      local tasks_done_count = meta.done_tasks
+      local tasks_total_count = meta.default_tasks + meta.active_tasks + meta.done_tasks
 
       -- change highlight if all the children tasks are done
       local hlmap = {
@@ -138,6 +212,21 @@ local setup_ufo = function()
           tasks_info = string.format("%s %s/%s", config.chars.task_info, tasks_done_count, tasks_total_count)
         end
       end
+
+      -- meta
+      if meta.total_time > 0 then
+        local format_time = function(seconds)
+          local hours = math.floor(seconds / 3600)
+          local minutes = math.floor((seconds % 3600) / 60)
+          local time = ""
+          if hours > 0 then time = time .. string.format("%dh", hours) end
+          if minutes > 0 then time = time .. string.format("%dm", minutes) end
+          return time
+        end
+        tasks_info = tasks_info .. string.format("  %s", format_time(meta.total_time))
+      end
+
+      -- padding
       local info_without_padding = " " .. tasks_info .. string.format(" %d lines", folded_lines_count)
       local padding_length = max_length - #info_without_padding - text_length
       local padding = ""
