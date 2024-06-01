88f8017075230bc3e6b81c836e674e950c07a55f
3rd <3rd@users.noreply.github.com>
2024-06-01 23:04:46 +0300

    nvim: sync

--- dotfiles/nvim/lua/modules/completion/supermaven.lua
+++ dotfiles/nvim/lua/modules/completion/supermaven.lua
@@ -4,8 +4,8 @@ return lib.module.create({
   hosts = { "spaceship", "macbook" },
   plugins = {
     {
-      -- "supermaven-inc/supermaven-nvim",
-      "Hashiraee/supermaven-nvim",
+      "supermaven-inc/supermaven-nvim",
+      -- "Hashiraee/supermaven-nvim",
       event = "VeryLazy",
       -- dir = lib.path.resolve(lib.env.dirs.vim.config, "plugins", "supermaven-nvim"),
       config = function()
@@ -17,7 +17,15 @@ return lib.module.create({
           ignore_filetypes = {
             dotenv = true,
             syslang = true,
-            -- markdown = true,
+            markdown = true,
+            help = true,
+            gitcommit = true,
+            gitrebase = true,
+            gitstatus = true,
+            yaml = true,
+            toml = true,
+            json = true,
+            text = true,
           },
           color = {
             suggestion_color = "#ffffff",
