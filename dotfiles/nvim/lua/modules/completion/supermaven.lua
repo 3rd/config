482b0161d3ee76570dc9c7bb525f4188ce83b4ac
3rd <3rd@users.noreply.github.com>
2024-07-18 11:39:43 +0300

    home/nvim: sync

--- dotfiles/nvim/lua/modules/completion/supermaven.lua
+++ dotfiles/nvim/lua/modules/completion/supermaven.lua
@@ -1,3 +1,29 @@
+local is_initialized = false
+
+local config = {
+  keymaps = {
+    accept_suggestion = "<C-l>",
+    clear_suggestion = "<C-]>",
+  },
+  ignore_filetypes = {
+    dotenv = true,
+    syslang = true,
+    markdown = true,
+    help = true,
+    gitcommit = true,
+    gitrebase = true,
+    gitstatus = true,
+    yaml = true,
+    toml = true,
+    json = true,
+    text = true,
+  },
+  color = {
+    suggestion_color = "#ffffff",
+    cterm = 244, -- "#ffffff",
+  },
+}
+
 return lib.module.create({
   name = "completion/supermaven",
   enabled = false,
@@ -5,33 +31,37 @@ return lib.module.create({
   plugins = {
     {
       "supermaven-inc/supermaven-nvim",
+      commit = "df3ecf7",
       -- "Hashiraee/supermaven-nvim",
       event = "VeryLazy",
       -- dir = lib.path.resolve(lib.env.dirs.vim.config, "plugins", "supermaven-nvim"),
-      config = function()
-        require("supermaven-nvim").setup({
-          keymaps = {
-            accept_suggestion = "<C-l>",
-            clear_suggestion = "<C-]>",
-          },
-          ignore_filetypes = {
-            dotenv = true,
-            syslang = true,
-            markdown = true,
-            help = true,
-            gitcommit = true,
-            gitrebase = true,
-            gitstatus = true,
-            yaml = true,
-            toml = true,
-            json = true,
-            text = true,
-          },
-          color = {
-            suggestion_color = "#ffffff",
-            cterm = 244, -- "#ffffff",
-          },
-        })
+    },
+  },
+  actions = {
+    {
+      "n",
+      "SuperMaven: Start",
+      function()
+        if not is_initialized then
+          is_initialized = true
+          require("supermaven-nvim").setup(config)
+          return
+        end
+        require("supermaven-nvim.api").start()
+      end,
+    },
+    {
+      "n",
+      "SuperMaven: Stop",
+      function()
+        require("supermaven-nvim.api").stop()
+      end,
+    },
+    {
+      "n",
+      "SuperMaven: Show logs",
+      function()
+        require("supermaven-nvim.api").show_log()
       end,
     },
   },
