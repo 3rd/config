1dbab1089f8a8ed423b0dd92e58dfdbb9ecf8ff5
3rd <3rd@users.noreply.github.com>
2024-08-07 01:47:00 +0300

    home: sync, add zz (zoxide | fzf | cd)

--- home-manager/programs/zoxide.nix
+++ home-manager/programs/zoxide.nix
@@ -1,6 +1,18 @@
 {
   programs.zoxide.enable = true;
 
-  programs.fish.shellAliases = { zq = "zoxide query -l"; };
+  programs.fish = {
+    functions = {
+      zz = {
+        body = ''
+          set path (zoxide query -l | fzf --preview 'zoxide query {}')
+          if test -n "$path"
+            cd "$path"
+          end
+        '';
+      };
+    };
+    shellAliases = { zq = "zoxide query -l"; };
+  };
 }
 
