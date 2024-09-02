8251846bea599002069541be0960174e7e985060
3rd <3rd@users.noreply.github.com>
2024-09-03 00:23:02 +0300

    home/nvim: sync

--- modules/audio.nix
+++ modules/audio.nix
@@ -15,11 +15,11 @@
       enable = true;
       extraConfig = {
         "monitor.bluez.properties" = {
-          "bluez5.enable-sbc-xq" = true;
-          "bluez5.enable-msbc" = true;
-          "bluez5.enable-hw-volume" = true;
-          "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
-          "bluez5.auto-connect" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+          # "bluez5.enable-sbc-xq" = true;
+          # "bluez5.enable-msbc" = true;
+          # "bluez5.enable-hw-volume" = true;
+          # "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+          # "bluez5.auto-connect" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
           "bluez5.profile" = "a2dp-sink";
           "bluez5.autoswitch-profile" = false;
         };
