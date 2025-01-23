901e35fc9ef45cae8c73a13942ca8792c8714af9
3rd <3rd@users.noreply.github.com>
2025-01-23 10:48:17 +0200

    home/nvim: sync

--- dotfiles/nvim/lua/modules/completion/blink.lua
+++ dotfiles/nvim/lua/modules/completion/blink.lua
@@ -96,25 +96,17 @@ return lib.module.create({
                 label = {
                   width = { fill = true, max = 60 },
                   text = function(ctx)
-                    local highlights_info = require("colorful-menu").highlights(ctx.item, vim.bo.filetype)
+                    local highlights_info = require("colorful-menu").blink_highlights(ctx)
                     if highlights_info ~= nil then
-                      return highlights_info.text
+                      return highlights_info.label
                     else
                       return ctx.label
                     end
                   end,
                   highlight = function(ctx)
-                    local highlights_info = require("colorful-menu").highlights(ctx.item, vim.bo.filetype)
+                    local highlights_info = require("colorful-menu").blink_highlights(ctx)
                     local highlights = {}
-                    if highlights_info ~= nil then
-                      for _, info in ipairs(highlights_info.highlights) do
-                        table.insert(highlights, {
-                          info.range[1],
-                          info.range[2],
-                          group = ctx.deprecated and "BlinkCmpLabelDeprecated" or info[1],
-                        })
-                      end
-                    end
+                    if highlights_info ~= nil then highlights = highlights_info.highlights end
                     for _, idx in ipairs(ctx.label_matched_indices) do
                       table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
                     end
